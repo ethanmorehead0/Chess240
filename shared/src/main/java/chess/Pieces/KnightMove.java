e1bde24bfb12e95a63e3702a281ea9e60b9e5e1e
ethan <ethanmorehead@gmail.com>
2024-01-22 22:42:15 -0700

    King and Knight

--- shared/src/main/java/chess/Pieces/KnightMove.java
+++ shared/src/main/java/chess/Pieces/KnightMove.java
@@ -8,27 +8,30 @@ import chess.ChessPosition;
 import java.util.ArrayList;
 
 
-public class KingMove {
+public class KnightMove {
 
 
-    public KingMove(ArrayList<ChessMove> a, ChessPosition myPosition, ChessBoard board){
+    public KnightMove(ArrayList<ChessMove> a, ChessPosition myPosition, ChessBoard board){
         int row = myPosition.getRow();
         int column = myPosition.getColumn();
+        int nextRow;
+        int nextColumn;
         ChessPosition positionTest; //position to test
         ChessPiece spaceTest; // space to test
+        int[][] iterator={{1,2},{2,1},{-1,2},{-2,1},{1,-2},{2,-1},{-1,-2},{-2,-1}};
 
-
-        for (int i=-1; i<=1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if((i==0&&j==0)||myPosition.getRow()+i+1<0||myPosition.getRow()+i+1>7||myPosition.getColumn()+j+1<0||myPosition.getColumn()+j+1>7){continue;}
-                positionTest = new ChessPosition(myPosition.getRow()+i+1, myPosition.getColumn()+j+1);
-                spaceTest = board.getPiece(positionTest);
-                if (spaceTest==null) {
-                    a.add(new ChessMove(myPosition, positionTest, null));
-                } else if (spaceTest.getTeamColor()!=board.getPiece(myPosition).getTeamColor()){
-                    a.add(new ChessMove(myPosition, positionTest, null));
-                }
+        for (int[] i: iterator) {
+            nextRow=myPosition.getRow()+i[0]+1;
+            nextColumn=myPosition.getColumn()+i[1]+1;
+            if(i[0]==0||i[1]==0||nextRow<=0||nextRow>8||nextColumn<=0||nextColumn>8){continue;}
+            positionTest = new ChessPosition(myPosition.getRow()+i[0]+1, myPosition.getColumn()+i[1]+1);
+            spaceTest = board.getPiece(positionTest);
+            if (spaceTest==null) {
+                a.add(new ChessMove(myPosition, positionTest, null));
+            } else if (spaceTest.getTeamColor()!=board.getPiece(myPosition).getTeamColor()){
+                a.add(new ChessMove(myPosition, positionTest, null));
             }
+
         }
         /*
         int[][] iterator ={{-1,-1},{-1,1},{1,-1},{1,1}};
@@ -44,8 +47,8 @@ public class KingMove {
             } else {
                 break;
             }*/
-            //System. out. println(column+i[1]*j);
-            //System. out. println(j);
+        //System. out. println(column+i[1]*j);
+        //System. out. println(j);
 
 
 
